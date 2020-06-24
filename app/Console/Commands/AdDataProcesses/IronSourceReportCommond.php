4247de2b3d90c3ba7a7b86c1d7615cd96da9cf8d
zhenliye <jianwuhen526@yeah.net>
2020-06-24 18:55:09 +0800

    is

--- app/Console/Commands/AdDataProcesses/IronSourceReportCommond.php
+++ app/Console/Commands/AdDataProcesses/IronSourceReportCommond.php
@@ -48,6 +48,7 @@ class IronSourceReportCommond extends Command
     public function handle()
     {
         set_time_limit(0);
+        ini_set("memory_limit",'3096M');
         // 入口方法
         $dayid = $this->argument('dayid')?$this->argument('dayid'):date('Y-m-d',strtotime('-1 day'));
         $appid = $this->argument('appid')?$this->argument('appid'):'';
