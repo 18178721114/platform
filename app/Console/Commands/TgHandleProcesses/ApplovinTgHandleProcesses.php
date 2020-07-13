3e32419d406656df1d45e13d0d296a3c4898311e
zhenliye <jianwuhen526@yeah.net>
2020-07-13 09:55:11 +0800

    报错信息排序

--- app/Console/Commands/TgHandleProcesses/ApplovinTgHandleProcesses.php
+++ app/Console/Commands/TgHandleProcesses/ApplovinTgHandleProcesses.php
@@ -276,16 +276,19 @@ class ApplovinTgHandleProcesses extends Command
             $error_log_arr = Service::shield_error($source_id,$error_log_arr);
 
             if (isset($error_log_arr['campaign_id']) && !empty($error_log_arr['campaign_id'])){
+                sort($error_log_arr['campaign_id']);
                 $campaign_id = implode(',',array_unique($error_log_arr['campaign_id']));
                 $error_msg_array[] = 'campaign_package_name匹配失败,ID为:'.$campaign_id;
                 $error_msg_mail[] = 'campaign_package_name匹配失败，ID为：'.$campaign_id;
             }
             if (isset($error_log_arr['country']) && !empty($error_log_arr['country'])){
+                sort($error_log_arr['country']);
                 $country = implode(',',array_unique($error_log_arr['country']));
                 $error_msg_array[] = '国家匹配失败,code为:'.$country;
                 $error_msg_mail[] = '国家匹配失败，code为：'.$country;
             }
             if (isset($error_log_arr['ad_type']) && !empty($error_log_arr['ad_type'])){
+                sort($error_log_arr['ad_type']);
                 $ad_type = implode(',',array_unique($error_log_arr['ad_type']));
                 $error_msg_array[] = '广告类型匹配失败,ID为:'.$ad_type;
                 $error_msg_mail[] = '广告类型匹配失败，ID为：'.$ad_type;
