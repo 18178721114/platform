453fd488fff3dee7c4c173a1adc0def3df397cbc
lixinglong <296694605@qq.com>
2020-07-22 15:35:33 +0800

    no message

--- app/Console/Commands/AdHandleProcesses/FacebookBiddingHandleProcesses.php
+++ app/Console/Commands/AdHandleProcesses/FacebookBiddingHandleProcesses.php
@@ -158,10 +158,7 @@ class FacebookBiddingHandleProcesses extends Command
         $error_detail_arr = [];//报错的 详细数据信息
         foreach ($info as $k => $v) {
         	$json_info = json_decode($v['json_data'],true);
-            $delivery_method = isset($json_info['delivery_method']) ? $json_info['delivery_method'] : '';
-            if ($delivery_method != 'standard'){
-                continue;
-            }
+
         	foreach ($app_list as $app_k => $app_v) {
         		if($json_info['placement'] == $app_v['ad_slot_id'] && ($json_info['appid'] == $app_v['platform_app_id'] || $json_info['appid'] == $app_v['publisher_id']) ){
         			$array[$k]['app_id'] = $app_v['app_id'];
